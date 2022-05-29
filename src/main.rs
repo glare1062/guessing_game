ff4ebb7d79c79d7a990cad084b6d0ff4da697f6f
Arunodh_105434 <Arunodh.Ranasinghe@axiatadigitallabs.com>
2022-05-29 20:10:22 +0530

    added error checking and loop functionality:

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 fn main() {
-    println!("Guess the number!");
 
-    let mut guess = String::new();
+    let secret_number =  rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {}", secret_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read number");
-    println!("GUessed {}",guess);
 
+    loop {
+        let mut guess = String::new();
+
+        println!("Guess the number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read number");
+
+        // let guess: u32 = guess.trim().parse().expect("type in a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("GUessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("COrrect guess");
+                break;
+            } ,
+        }
+    }
 
 
 
